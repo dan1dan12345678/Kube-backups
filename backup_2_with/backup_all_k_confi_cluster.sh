e70476b87795f4ecf8590db321ade85e00dfa224
dan311027@gmail.com <dan311027@gmail.com>
2024-01-17 13:02:21 +0100

    first commit

---
+++ backup_2_with/backup_all_k_confi_cluster.sh
@@ -0,0 +1,4 @@
+for n in $(kubectl get -o=name pvc,configmap,ingress,service,secret,deployment,statefulset,hpa,job,cronjob | grep -v 'secret/default-token')
+do
+    kubectl get -o=yaml --export $n > $(dirname $n)_$(basename $n).yaml
+done
